c3276446547817fa070f275ea6c53c280f667ce1
rommel667 <rommel667@gmail.com>
2021-05-04 18:01:04 +0800

    :sparkles: initial component without test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
+import './app.scss'
 
 function App() {
   return (
     <div className="App">
-     Test
+     <Header/>
     </div>
   );
 }
